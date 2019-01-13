52c25f27da583cb0d649b4de05073956f3175725
Sheila Stephen <sheila.je@gmail.com>
2019-01-13 07:41:20 -0800

    add methods to find life expectancy of user in other worlds

--- spec/galacticAgeCalculator-spec.js
+++ spec/galacticAgeCalculator-spec.js
@@ -1,24 +1,35 @@
 import { Age } from './../src/galacticAgeCalculator.js';
 
 describe ( 'Age', function() {
-var newAge;
+let newAge, birthDate, currentDate, test;
+
   beforeEach(function() {
-    newAge = new Age(25);
+    let birthDate = new Date("04-06-1983");
+    let currentDate = new Date("11-01-2019");
+    let newAge = new Age(currentDate, birthDate);
+    let test = newAge.calculateEarthYears();
+    //console.log("test :", test);
+  });
+      //console.log("test");
+  it('should return the earth years', function() {
+     //let earthY = newAge.calculateEarthYears();
+     //console.log("earthY : ", earthY);
+    expect(newAge.userAge).toEqual(36);
   });
 
   it('should return the mercury Age', function() {
-    expect(newAge.calculateMercuryYears()).toEqual(104);
+    expect(newAge.calculateMercuryYears()).toEqual(150);
   });
 
   it('should return the venus Age', function() {
-    expect(newAge.calculateVenusYears()).toEqual(40);
+    expect(newAge.calculateVenusYears()).toEqual(58);
   });
 
   it('should return the mars Age', function() {
-    expect(newAge.calculateMarsYears()).toEqual(13);
+    expect(newAge.calculateMarsYears()).toEqual(19);
   });
 
   it('should return the jupiter Age', function() {
-    expect(newAge.calculateJupiterYears()).toEqual(2);
+    expect(newAge.calculateJupiterYears()).toEqual(3);
   });
-});
+  });
